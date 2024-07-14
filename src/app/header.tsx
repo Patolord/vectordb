c3023d72e64469c5ec04a5293531ce751bc454f6
patolord <rodrigo_luizp@hotmail.com>
2024-07-13 22:58:51 -0300

    feat: Add lazy loading for images and fix signup form bug

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -14,12 +14,22 @@ import {
 } from "@/components/ui/dialog";
 import { SignInWithPassword } from "./(auth)/SignIn";
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="z-10 relative dark:bg-slate-900 bg-slate-50 py-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div>LOGO</div>
+        <div className="flex gap-12 items-center">
+          <Link href="/">
+            <div>LOGO</div>
+          </Link>
+          <nav>
+            <Link href="/" className="hover:text-slate-400">
+              Documents
+            </Link>
+          </nav>
+        </div>
 
         <div>
           <AuthLoading>Carregando...</AuthLoading>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,26 +11,68 @@ import { api } from "../../convex/_generated/api";
 import DocumentCard from "./documentCard";
 
 import UploadDocumentButton from "./uploadDocumentButton";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
   const documents = useQuery(api.documents.getDocuments);
 
   return (
     <main className="p-24 space-y-8">
-      <AuthLoading>Carregando...</AuthLoading>
+      <AuthLoading>
+        {documents === undefined && (
+          <div className="grid grid-cols-4 gap-8">
+            {new Array(8).fill("").map((_, i) => (
+              <Card
+                key={i}
+                className="h-[200px] p-6 flex flex-col justify-between"
+              >
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="w-[80px] h-[40px] rounded" />
+              </Card>
+            ))}
+          </div>
+        )}
+      </AuthLoading>
       <Unauthenticated>{""}</Unauthenticated>
       <Authenticated>
-        <div className="flex justify-between item s-center">
+        <div className="flex justify-between items-center">
           <h1 className="text-4xl font-bold">My Documents</h1>
 
           <UploadDocumentButton />
         </div>
 
-        <div className="grid grid-cols-4 gap-8">
-          {documents?.map((doc) => (
-            <DocumentCard document={doc} key={doc._id} />
-          ))}
-        </div>
+        {documents === undefined && (
+          <div className="grid grid-cols-4 gap-8">
+            {new Array(8).fill("").map((_, i) => (
+              <Card
+                key={i}
+                className="h-[200px] p-6 flex flex-col justify-between"
+              >
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="h-[20px] rounded" />
+                <Skeleton className="w-[80px] h-[40px] rounded" />
+              </Card>
+            ))}
+          </div>
+        )}
+
+        {documents && documents.length > 0 && (
+          <div className="grid grid-cols-4 gap-8">
+            {documents?.map((doc) => (
+              <DocumentCard document={doc} key={doc._id} />
+            ))}
+          </div>
+        )}
+
+        {documents && documents.length === 0 && (
+          <div className="flex flex-col justify-center items-center gap-8">
+            <h2> No documents</h2>
+          </div>
+        )}
       </Authenticated>
     </main>
   );
